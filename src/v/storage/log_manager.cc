9fcd8966dbb9d04de028ec4a27c64376a55788dc
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-02-13 15:27:34 -0500

    `storage`: plumb `maybe_abort_compaction()` into compaction utils

--- src/v/storage/log_manager.cc
+++ src/v/storage/log_manager.cc
@@ -352,7 +352,8 @@ log_manager::housekeeping_scan(model::timestamp collection_threshold) {
           _config.compaction_priority,
           _abort_source,
           std::move(ntp_sanitizer_cfg),
-          _compaction_hash_key_map.get()));
+          _compaction_hash_key_map.get(),
+          &_disk_space_alert));
         _probe->housekeeping_log_processed();
 
         // bail out of compaction early in order to get back to gc
