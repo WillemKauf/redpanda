ff59bf915b1984f04b805fb6e73ebb0caeebb1f8
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-06-24 16:00:20 -0400

    `cluster`: add `shard_placement_table::get_probe()`
    For use in testing.

--- src/v/cluster/shard_placement_table.h
+++ src/v/cluster/shard_placement_table.h
@@ -322,6 +322,7 @@ public:
       remake_partition_state remake_state,
       model::revision_id expected_log_rev);
 
+    probe* get_probe() const { return _probe.get(); }
 
 private:
     void assert_is_assignment_shard() const;
