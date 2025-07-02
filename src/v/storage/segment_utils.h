a87350a1e92a02929f5a0d1cdbc4f80861221e7a
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-07-02 12:44:33 -0400

    `storage`: add `log::max_removed_offset()`
    This is another necessary piece of information to ensure divergence free
    recovery.
    
    If a round of recovery begins at an offset below `log->max_removed_offset()`,
    there is the potential for divergence across replicas due to removed state
    (i.e removed tombstone records or transactional batches) not being seen by
    the follower.
    
    The intention here is to perform one initial check at the start of recovery
    (at time of `recovery_stm` construction) between the `follower->next_index`
    and the `log->max_removed_offset()`. If `next < max_removed`, we must issue
    a reset of the learner's state and begin recovery from the `log` start offset
    once more.
    
    The `max_removed_offset` is adjusted every time a tombstone or transactional
    batch is removed due to the `delete.retention.ms` horizon being exceeded.
    De-duplication (i.e removal due to a later record with the same key existing in
    the log) does not affect its value.
    
    Because this piece of information (unlike `earliest_removable_timestamp()`) is
    not reconstructable from iteration over the `segment`s in a `log`, it must be
    persisted in the `kvstore` per `partition`.

--- src/v/storage/segment_utils.h
+++ src/v/storage/segment_utils.h
@@ -58,6 +58,7 @@ ss::future<compaction_result> self_compact_segment(
   storage::readers_cache&,
   storage::storage_resources&,
   ss::sharded<features::feature_table>& feature_table,
+  kvstore& kvs,
   bool force_compaction = false);
 
 /// \brief, rebuilds a given segment's compacted index. This method acquires
@@ -112,7 +113,8 @@ ss::future<compaction_result> concatenate_and_rebuild_target_segment(
   storage::readers_cache& readers_cache,
   storage_resources& resources,
   ss::sharded<features::feature_table>& feature_table,
-  mutex& segment_rewrite_lock);
+  mutex& segment_rewrite_lock,
+  kvstore& kvs);
 
 ss::future<> write_concatenated_compacted_index(
   std::filesystem::path,
@@ -218,7 +220,8 @@ ss::future<storage::index_state> do_copy_segment_data(
   storage::compaction_config,
   storage::probe&,
   ss::rwlock::holder,
-  storage_resources&);
+  storage_resources&,
+  kvstore&);
 
 ss::future<> do_swap_data_file_handles(
   std::filesystem::path compacted,
@@ -235,10 +238,12 @@ float random_jitter(jitter_percents);
 enum class kvstore_key_type : int8_t {
     start_offset = 0,
     clean_segment = 1,
+    max_removed_offset = 2,
 };
 
 bytes start_offset_key(model::ntp ntp);
 bytes clean_segment_key(model::ntp ntp);
+bytes max_removed_offset_key(model::ntp ntp);
 
 struct clean_segment_value
   : serde::envelope<
@@ -370,7 +375,9 @@ ss::future<bool> should_keep(
   model::offset segment_last_offset,
   bool past_tombstone_delete_horizon,
   bool& may_have_tombstone_records,
-  bool& has_tx_batches) {
+  [[maybe_unused]] bool past_tx_delete_horizon,
+  bool& has_tx_batches,
+  model::offset& max_removed_offset) {
     auto compaction_placeholder_enabled = feature_table.local().is_active(
       features::feature::compaction_placeholder_batch);
     auto is_last_batch = b.last_offset() == segment_last_offset;
@@ -398,6 +405,8 @@ ss::future<bool> should_keep(
 
     // Deal with tombstone record removal
     if (r.is_tombstone() && past_tombstone_delete_horizon) {
+        max_removed_offset = b.base_offset()
+                             + model::offset_delta(r.offset_delta());
         pb.add_removed_tombstone();
         co_return false;
     }
@@ -422,5 +431,11 @@ ss::future<bool> should_keep(
 
     co_return keep;
 }
+
+std::optional<model::offset>
+read_max_removed_offset(kvstore&, const model::ntp&);
+
+ss::future<>
+write_max_removed_offset(kvstore&, const model::ntp&, model::offset);
 
 } // namespace storage::internal
