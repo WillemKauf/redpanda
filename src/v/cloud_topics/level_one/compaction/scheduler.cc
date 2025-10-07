d3be93fbd462ff07d44a3d91f8609dac05d83189
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-10-07 16:44:44 -0400

    add compaction scheduler

---
+++ src/v/cloud_topics/level_one/compaction/scheduler.cc
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+#include "cloud_topics/level_one/compaction/scheduler.h"
+
+#include "cloud_topics/level_one/compaction/committer.h"
+#include "cloud_topics/level_one/compaction/committing_policy.h"
+#include "cloud_topics/level_one/compaction/log_collector.h"
+#include "cloud_topics/level_one/compaction/log_sampler.h"
+#include "cloud_topics/level_one/compaction/logger.h"
+#include "cloud_topics/level_one/compaction/meta.h"
+#include "cloud_topics/level_one/compaction/scheduling_policies.h"
+#include "config/configuration.h"
+#include "container/chunked_circular_buffer.h"
+#include "model/fundamental.h"
+#include "ssx/future-util.h"
+
+namespace cloud_topics::l1 {
+
+compaction_scheduler::compaction_scheduler(
+  compaction_cluster_state state,
+  std::unique_ptr<scheduling_policy> policy,
+  ss::sharded<file_io>* io,
+  ss::sharded<l1::replicated_metastore>* metastore)
+  : _io(io)
+  , _metastore(metastore)
+  , _metadata_cache(state.metadata_cache)
+  , _log_collector(make_default_log_collector(this, state))
+  , _log_sampler(&_metastore->local(), &state.metadata_cache->local())
+  , _scheduling_policy(std::move(policy))
+  , _worker_manager(_cached_metadata, io, metastore, &_committer)
+  , _compaction_interval(
+      config::shard_local_cfg().log_compaction_interval_ms.bind())
+  , _cached_metadata(_scheduling_policy->get_cmp_t()) {
+    _compaction_interval.watch([this]() { _sem.signal(); });
+}
+
+bool compaction_scheduler::is_managed(const model::ntp& ntp) const noexcept {
+    auto it = _ntp_to_tidp.find(ntp);
+    if (it == _ntp_to_tidp.end()) {
+        return false;
+    }
+
+    auto& tidp = it->second;
+
+    return _logs.contains(tidp);
+}
+
+void compaction_scheduler::manage_partition(
+  const model::ntp& ntp,
+  const model::topic_id_partition& tidp,
+  std::string_view ctx) {
+    vlog(
+      compaction_log.info, "Asked to manage compacted CTP: {} ({})", ntp, ctx);
+    auto [it, success] = _logs.insert(
+      ss::make_lw_shared<log_compaction_meta>(tidp, ntp));
+    _logs_list.push_back(*it->get());
+    _ntp_to_tidp.emplace(ntp, tidp);
+    vassert(
+      success, "Could not manage compacted CTP {} (concurrency issue?)", ntp);
+}
+
+ss::future<>
+compaction_scheduler::unmanage_partition(model::ntp ntp, std::string_view ctx) {
+    auto tidp_entry = _ntp_to_tidp.extract(ntp);
+    if (!tidp_entry.has_value()) {
+        co_return;
+    }
+
+    auto& tidp = tidp_entry->second;
+
+    vlog(
+      compaction_log.info,
+      "Asked to unmanage compacted CTP: {} ({})",
+      tidp,
+      ctx);
+
+    auto handle_opt = _logs.extract(tidp);
+    if (!handle_opt) {
+        co_return;
+    }
+
+    auto handle = std::move(handle_opt).value();
+
+    auto close_fut = handle->gate.close();
+
+    // Request that compaction of this CTP be stopped, if in flight.
+    co_await _worker_manager.request_stop_compaction(handle);
+
+    co_await std::move(close_fut);
+}
+
+void compaction_scheduler::start_bg_loop() {
+    ssx::repeat_until_gate_closed_or_aborted(_gate, _as, [this] {
+        return scheduling_loop().handle_exception(
+          [](const std::exception_ptr& e) {
+              auto log_level = ssx::is_shutdown_exception(e)
+                                 ? ss::log_level::debug
+                                 : ss::log_level::warn;
+              vlogl(
+                compaction_log,
+                log_level,
+                "Encountered exception in main loop: {}",
+                e);
+          });
+    });
+}
+
+ss::future<> compaction_scheduler::scheduling_loop() {
+    vlog(compaction_log.debug, "Starting compaction scheduling loop");
+    while (!_gate.is_closed() && !_as.abort_requested()) {
+        auto compaction_interval = _compaction_interval();
+        try {
+            co_await _sem.wait(
+              _compaction_interval(), std::max(_sem.current(), size_t(1)));
+        } catch (const ss::semaphore_timed_out&) {
+            // Fall through
+        }
+
+        if (compaction_interval != _compaction_interval()) {
+            // Cluster config was changed while waiting.
+            continue;
+        }
+
+        co_await _log_sampler.sample_logs(_logs, _logs_list, _cached_metadata);
+
+        co_await _worker_manager.alert_workers();
+    }
+}
+
+ss::future<> compaction_scheduler::start() {
+    co_await _committer.start(
+      ss::sharded_parameter([] { return make_default_committing_policy(); }),
+      ss::sharded_parameter([this] { return &_metastore->local(); }),
+      ss::sharded_parameter([this] { return &_io->local(); }));
+    co_await _committer.invoke_on_all(&compaction_committer::start);
+    co_await _worker_manager.start();
+    co_await _log_collector->start();
+    start_bg_loop();
+}
+
+ss::future<> compaction_scheduler::stop() {
+    vlog(compaction_log.debug, "Stopping compaction scheduling loop");
+    _as.request_abort();
+    _sem.broken();
+
+    // Stop making new jobs.
+    auto close_fut = _gate.close();
+
+    // Stop collecting logs.
+    co_await _log_collector->stop();
+
+    // Close gates for all logs.
+    static constexpr size_t max_concurrent_close = 1024;
+    co_await ss::max_concurrent_for_each(
+      _logs.begin(), _logs.end(), max_concurrent_close, [](auto& log) {
+          return log->gate.close();
+      });
+    _logs.clear();
+
+    // It is only safe to stop the worker_manager once all gates have been
+    // closed.
+    co_await _worker_manager.stop();
+
+    // Stop committing data.
+    co_await _committer.stop();
+
+    co_await std::move(close_fut);
+}
+
+std::unique_ptr<compaction_scheduler> make_default_compaction_scheduler(
+  compaction_cluster_state state,
+  ss::sharded<file_io>* io,
+  ss::sharded<replicated_metastore>* metastore) {
+    return std::make_unique<compaction_scheduler>(
+      state, make_default_scheduling_policy(), io, metastore);
+}
+
+} // namespace cloud_topics::l1
---
+++ src/v/cloud_topics/level_one/compaction/scheduler.h
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+#pragma once
+
+#include "cloud_topics/level_one/common/file_io.h"
+#include "cloud_topics/level_one/compaction/committer.h"
+#include "cloud_topics/level_one/compaction/log_collector.h"
+#include "cloud_topics/level_one/compaction/log_sampler.h"
+#include "cloud_topics/level_one/compaction/meta.h"
+#include "cloud_topics/level_one/compaction/scheduling_policies.h"
+#include "cloud_topics/level_one/compaction/worker_manager.h"
+#include "cloud_topics/level_one/metastore/replicated_metastore.h"
+#include "cluster/metadata_cache.h"
+#include "config/property.h"
+#include "container/chunked_circular_buffer.h"
+#include "model/fundamental.h"
+#include "ssx/semaphore.h"
+
+#include <queue>
+
+namespace cloud_topics::l1 {
+
+/*
+ * Responsible for scheduling compaction for cloud topic partitions.
+ */
+class compaction_scheduler {
+public:
+    compaction_scheduler(
+      compaction_cluster_state,
+      std::unique_ptr<scheduling_policy>,
+      ss::sharded<file_io>*,
+      ss::sharded<l1::replicated_metastore>*);
+
+    // Starts the contained `_log_collector`, `_worker_manager`, and the
+    // backgrounded scheduling loop.
+    ss::future<> start();
+
+    // Shuts down concurrency primitives, thereby stopping the backgrounded
+    // scheduling loop, stops the `_log_collector`, requests inflight compaction
+    // jobs in the `_worker_manager` be stopped, drains the managed partition
+    // log list, and finally shuts down the `_worker_manager` once it is safe to
+    // do so.
+    ss::future<> stop();
+
+    // Returns `true` iff the provided `tidp` is managed by this scheduler.
+    bool is_managed(const model::ntp&) const noexcept;
+
+    // Pushes a new `tidp` to be managed by this scheduler to the list of
+    // `tidp`s. It is the caller's responsibility to ensure the partition is
+    // not already managed by this scheduler.
+    void manage_partition(
+      const model::ntp&, const model::topic_id_partition&, std::string_view);
+
+    // Removes the `tidp` from the list of managed partitions. No-ops if the
+    // provided `tidp` is not managed by this scheduler. Because the `tidp`
+    // may be undergoing an inflight compaction, this function will block until
+    // it is complete (an early stop is requested by this function).
+    ss::future<> unmanage_partition(model::ntp, std::string_view);
+
+private:
+    // Starts the backgrounded scheduling loop.
+    void start_bg_loop();
+
+    // The main compaction loop. Invoked in a background fiber until `_as` has
+    // an abort requested or the `_gate` is closed.
+    ss::future<> scheduling_loop();
+
+    // Samples managed logs and schedules compactions.
+    ss::future<> schedule_some();
+
+private:
+    // Pointer to sharded `file_io` held by `app`. Used by the `worker_manager`
+    // for writing to local files and by the `committer` for writing to cloud
+    // storage.
+    ss::sharded<file_io>* _io;
+
+    // Pointer to metastore.
+    ss::sharded<replicated_metastore>* _metastore;
+
+    // Pointer to metadata_cache.
+    ss::sharded<cluster::metadata_cache>* _metadata_cache;
+
+private:
+    // Responsible for pushing logs to manage/unmanage to this scheduler.
+    std::unique_ptr<log_collector> _log_collector;
+
+    // Responsible for collecting compaction metadata (see: `log_info` in
+    // `meta.h`) for managed logs during a scheduling loop.
+    log_sampler _log_sampler;
+
+    // Responsible for sorting logs collected by sampler for compaction.
+    std::unique_ptr<scheduling_policy> _scheduling_policy;
+
+    // Responsible for dispatching compaction jobs to per-shard workers.
+    worker_manager _worker_manager;
+
+    // Responsible for committing updates from sharded jobs ran on the
+    // `worker_manager` to the metastore and uploading compacted objects to
+    // cloud storage.
+    ss::sharded<compaction_committer> _committer;
+
+    // The interval on which compaction loop is executed.
+    config::binding<std::chrono::milliseconds> _compaction_interval;
+
+    // This semaphore is used as a way to signal a change to
+    // `log_compaction_interval_ms` during the `wait()` operation in the main
+    // scheduling loop.
+    ssx::semaphore _sem{0, "cloud_topics::compaction::scheduling_loop"};
+
+    ss::abort_source _as;
+    ss::gate _gate;
+
+    // Set of logs this scheduler is responsible for issuing compaction jobs
+    // for.
+    logs_type_t _logs;
+
+    // Intrusive list of logs this scheduler is responsible for issuing
+    // compaction jobs for.
+    log_list_t _logs_list;
+
+    // Container of pointers to logs in `_logs/_logs_list` which have sampled
+    // metadata available, i.e `log->info_and_ts` is guaranteed to have a value.
+    log_compaction_queue _cached_metadata;
+
+    // TODO: remove this once more cluster objects speak `topic_id_partition`.
+    chunked_hash_map<model::ntp, model::topic_id_partition> _ntp_to_tidp;
+};
+
+std::unique_ptr<compaction_scheduler> make_default_compaction_scheduler(
+  compaction_cluster_state,
+  ss::sharded<file_io>*,
+  ss::sharded<replicated_metastore>*);
+
+} // namespace cloud_topics::l1
