2aa3448a3a37cbd5ede2dbed42308a0184df00e1
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-10-10 13:17:02 -0400

    `compaction`: add exception safety to `reducer`
    To ensure `sink::finalize()` is called even if exceptions
    are thrown at any point in the compaction process.

--- src/v/compaction/reducer.cc
+++ src/v/compaction/reducer.cc
@@ -11,26 +11,38 @@
 
 #include <seastar/core/coroutine.hh>
 
+#include <exception>
+
 namespace compaction {
 
 ss::future<> compaction::sliding_window_reducer::run() && {
-    // Step 0: Initialize source
-    co_await _src->initialize();
+    std::exception_ptr eptr;
+    try {
+        // Step 0: Initialize source
+        co_await _src->initialize();
 
-    // Step 1: Perform map building pass.
-    co_await ss::repeat([this]() { return _src->map_building_iteration(); });
-
-    // Step 2: Initialize sink
-    bool should_deduplicate = co_await _sink->initialize(*_src);
-
-    if (should_deduplicate) {
-        // Step 3: Perform de-duplication pass.
+        // Step 1: Perform map building pass.
         co_await ss::repeat(
-          [this]() { return _src->deduplication_iteration(*_sink); });
+          [this]() { return _src->map_building_iteration(); });
+
+        // Step 2: Initialize sink
+        bool should_deduplicate = co_await _sink->initialize(*_src);
+
+        if (should_deduplicate) {
+            // Step 3: Perform de-duplication pass.
+            co_await ss::repeat(
+              [this]() { return _src->deduplication_iteration(*_sink); });
+        }
+    } catch (...) {
+        eptr = std::current_exception();
     }
 
     // Done!
     co_await _sink->finalize();
+
+    if (eptr) {
+        std::rethrow_exception(eptr);
+    }
 }
 
 } // namespace compaction
