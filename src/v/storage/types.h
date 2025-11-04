12e940c9f40ce6387141012715a889f70c9b393e
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-11-04 09:05:58 -0500

    `storage`: add `max_tx_remove_offset` to `compaction_config`
    Split `max_tombstone_remove_offset` into two logical components-
    one only limited by coordinated compaction, and one limited by
    both the offset provided by coordinated compaction and `tx_snapshot_offset`.
    
    This is to ensure tombstone removal is not held back by snapshots, and
    that transaction markers are not removed prematurely.

--- src/v/storage/types.h
+++ src/v/storage/types.h
@@ -478,6 +478,7 @@ struct housekeeping_config {
       std::optional<size_t> max_bytes_in_log,
       model::offset max_collect_offset,
       model::offset max_tombstone_remove_offset,
+      model::offset max_tx_remove_offset,
       std::optional<std::chrono::milliseconds> tombstone_retention_ms,
       std::optional<std::chrono::milliseconds> tx_retention_ms,
       std::chrono::milliseconds min_lag_ms,
@@ -495,6 +496,7 @@ struct housekeeping_config {
           as,
           san_cfg,
           key_map);
+        cfg.compact.max_tx_remove_offset = max_tx_remove_offset;
         return cfg;
     }
 
